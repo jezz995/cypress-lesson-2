c40449784bf3bd02b637a9aef14b380e3459bc79
{jezz995} <{jezz.veranda995@gmail.com}>
2023-08-22 21:32:40 +0700

    test2

---
+++ cypress/e2e/test2.cy.js
@@ -0,0 +1,43 @@
+/// <reference types ="cypress" />
+
+describe("Hrm web test login and add employee", () => {
+  beforeEach(function () {
+    cy.visit("https://opensource-demo.orangehrmlive.com/");
+  });
+
+  it("scenario-1", () => {
+    cy.get(
+      ":nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input"
+    ).type("Admin");
+    cy.get(
+      ":nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input"
+    ).type("admin123");
+    cy.get(".oxd-button").click();
+    ///open demo web the type password and username
+    cy.contains("Admin").should("be.visible").click();
+    cy.contains("Add").click();
+    cy.get(".oxd-autocomplete-text-input > input").type("Peter Mac Anderson");
+    cy.contains("Peter Mac Anderson").click();
+    cy.get(
+      ":nth-child(4) > .oxd-input-group > :nth-child(2) > .oxd-input"
+    ).type("Asolole12345");
+    cy.get(
+      ".user-password-cell > .oxd-input-group > :nth-child(2) > .oxd-input"
+    ).type("asolole12345");
+    cy.get(
+      ":nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input"
+    ).type("asolole12345");
+    cy.get(
+      ":nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper > .oxd-select-text > .oxd-select-text-input"
+    ).click();
+    cy.wait(1500);
+    cy.contains("ESS").click();
+    cy.get(
+      ":nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper > .oxd-select-text > .oxd-select-text-input"
+    ).click();
+    cy.wait(1500);
+    cy.contains("Enabled").click();
+    cy.contains("Save").click();
+    cy.wait(2000);
+  });
+});
